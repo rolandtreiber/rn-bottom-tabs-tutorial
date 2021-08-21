05a08441f7b012a1f30e2776aeea121a9ed3c150
Roland Treiber <rolandtreiber@gmail.com>
2021-08-21 07:14:39 +0100

    Implementation

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import styled from "styled-components/native";
+import MyCustomIconSet from './src/assets/fonts/MyIconSet.ttf'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    'MyCustomIconSet': MyCustomIconSet,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const icons = {
+    carrot: "",    // &#xe900;
+    tea: "",       // &#xe901;
+    coffee: "",    // &#xe902;
+    pineapple: "", // &#xe903;
+    cherries: "",  // &#xe904;
+  }
+
+  if (!fontsLoaded) {
+      return <AppLoading />;
+  } else {
+
+    const Container = styled.View`
+      flex: 1;
+      align-items: center;
+      justify-content: center;
+    `
+
+    const Row = styled.View`
+      flex-direction: row;
+    `
+
+    const LargeText = styled.Text`
+      font-size: 24px;
+    `
+
+    const Note = styled.Text`
+      font-size: 12px;
+      color: gray;
+      text-align: center;
+      padding:10px;
+    `    
+
+    const LargeIcon = styled.Text`
+      color: ${props => props.color};
+      font-family: "MyCustomIconSet";
+      font-size: 42px;
+      padding: 5px;
+    `
+
+    return (
+      <Container>
+        <Row>
+          <LargeText>
+            Custom Icons
+          </LargeText>
+        </Row>
+        <Row>
+          <LargeIcon color='orange'>{icons.carrot}</LargeIcon>
+          <LargeIcon color='green'>{icons.tea}</LargeIcon>
+          <LargeIcon color='brown'>{icons.coffee}</LargeIcon>
+          <LargeIcon color='orange'>{icons.pineapple}</LargeIcon>
+          <LargeIcon color='red'>{icons.cherries}</LargeIcon>
+        </Row>
+        <Row>
+          <Note>
+            You can assign color to them just like you would with any other text. Sweet :)
+          </Note>
+        </Row>        
+      </Container>
+    );
+  }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Using custom icon font in React Native
+
+This repo is support for a [medium article](https://medium.com/@hello-85764/how-to-use-custom-icon-font-in-react-native) on the same topic.
+
+## Usage
+
+- Clone the repository
+- Install packages `npm install`
+- Run the application `npm start`
+
+It is assumed that you have your React Native dev environment setup.
+
+### How to create your own icon set
+
+The main purpose of using an icon font instead of images or svg-s is that your icons behave just like regular characters with emphasis on size and color.
+
+That said, you want to use single colour svg-s as basis, that you may create yourself or acquire from somewhere.
+
+For plenty of free icons, check our [Flaticon](https://flaticon.com)
+
+To convert your icons to a font, my recommended tool is [Icomoon](https://icomoon.io/)
+
+#### The Author
+
+Please feel free to check out my website on [thecaringdeveloper.com](https://thecaringdeveloper.com)
+If you have any question, drop me a message on [LinkedIn](https://www.linkedin.com/in/roland-treiber) or via [email](mailto:hello@thecaringdeveloper.com)
